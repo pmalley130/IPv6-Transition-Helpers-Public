c91b23124c3d629a6de0b976bd273aabb06daa5a
pmalley130 <56446138+pmalley130@users.noreply.github.com>
2025-03-08 14:03:21 -0500

    Add files via upload
    Added functions Add-Address Object and Add-AddressGroup

---
+++ API/Add-AddressGroup.ps1
@@ -0,0 +1,46 @@
+﻿##############################
+#     By Patrick Malley      #
+#          3/7/25            #
+##############################
+
+
+function Add-AddressGroup { #function to add address group to a firewall
+    param(
+            [string]$fwfqdn, #fqdn of firewall
+            [string]$groupName, #name of address group
+            [string[]]$addresses, #array of addressobjects to be included in group
+            [string]$vdom
+        )
+
+    
+    if ($addresses[0].Contains(':')){ #determine if it's ipv4 or 6, set URL and API payload
+            $apiURL = "https://" + $fwfqdn + "/api/v2/cmdb/firewall/addrgrp6/?vdom=" + $vdom #set url for ipv6
+    } 
+    else {
+            $apiURL = "https://" + $fwfqdn + "/api/v2/cmdb/firewall/addrgrp/?vdom=" + $vdom #set url for ipv4
+    }
+
+    $members=@() #create an array for the address objects and fill it with separate hashtables for each 
+    foreach ($address in $addresses){
+        $members += @{name=$address}
+    }
+
+    $data = [ordered]@{#create the data to be sent via API
+        name="API test"
+        member=$members
+    }
+
+    $dataJson = $data | ConvertTo-Json #REST API only takes json so format it
+    
+    try { #make the API call
+        $answer = (Invoke-RestMethod -method Post -uri $apiURL -Body $dataJson -Headers $headers)
+    } catch [System.Net.WebException] {
+        $hostMSG = $apiURL + " is returning error" + $answer.status
+    }
+        
+    return $answer.mkey + " " + $answer.status #let console know whether it succeeded
+}
+
+
+
+
---
+++ API/Add-AddressObject.ps1
@@ -0,0 +1,46 @@
+﻿##############################
+#     By Patrick Malley      #
+#          3/7/25            #
+##############################
+
+
+function Add-AddressObject { #function to add address object to a firewall
+    param(
+            [string]$fwfqdn, #fqdn of firewall
+            [string]$objectName, #name of address object
+            [string]$address, #ip address 
+            [string]$vdom
+        )
+
+    if ($address.Contains(':')){ #determine if it's ipv4 or 6, set URL and API payload
+            $apiURL = "https://" + $fwfqdn + "/api/v2/cmdb/firewall/address6/?vdom=" + $vdom #set url for ipv6
+            
+            $body = [ordered]@{ #build data needed for object
+                name=$objectName
+                ip6=$address
+            }
+    } 
+    else {
+            $apiURL = "https://" + $fwfqdn + "/api/v2/cmdb/firewall/address/?vdom=" + $vdom #set url for ipv4
+
+            $body = [ordered]@{ #build data for IPv4, TODO: logic to parse CIDR and generate mask instead of assuming /32
+                name=$objectName
+                subnet=$address + " 255.255.255.255"
+            }
+    }
+
+    $body.Add("comment","added by API")
+
+    $bodyJson = $body | ConvertTo-Json #convert to format that the API can handle
+
+    try { #make the api call
+        $answer = (Invoke-RestMethod -method Post -uri $apiURL -Body $bodyJson -Headers $headers)
+    } catch [System.Net.WebException] {
+        $hostMSG = $apiURL + " is returning error" + $answer.status
+    }
+        
+    return $answer.mkey + " " + $answer.status #let console know whether it succeeded
+}
+
+
+
